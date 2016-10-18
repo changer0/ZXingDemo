c762eb0b8813b39d1724c1b28879ff9422512d84
张鹿鹿 <zhanglulu1007@foxmail.com>
2016-10-18 23:21:40 +0800

    利用Zxing实现二维码扫描 ...

--- app/src/main/java/com/lulu/zxingdemo/MainActivity.java
+++ app/src/main/java/com/lulu/zxingdemo/MainActivity.java
@@ -1,7 +1,12 @@
 package com.lulu.zxingdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.lulu.zxing.decoding.Intents;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnZXing(View view) {
+        Intent intent = new Intent( Intents.Scan.ACTION);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (data != null) {
+                String str = data.getStringExtra("result");
+                Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
